6ce112b04d7ddf2295962915c31fc9f12834a254
Eric Stine <ericstine@Erics-MacBook-Pro.local>
2023-09-08 04:17:33 -0400

    went back to recursive dfs

--- BFSvsDFS.py
+++ BFSvsDFS.py
@@ -9,26 +9,30 @@ CSV_FILE_EXTENSION = '.csv'
 
 # Error handling constants
 MAX_TRAVERSAL_QUEUE_SIZE = 10000  # Adjust as necessary
-TRAVERSAL_TIMEOUT_SECONDS = 60  # Adjust as necessary in seconds
+TRAVERSAL_TIMEOUT_SECONDS = 900  # Adjust as necessary in seconds
 
 
 # Section: Input and Output utilities
 def get_input(prompt: str) -> str:
+    """
+    Gets user input with a given prompt.
+
+    :param prompt: The prompt to display to the user
+    :return: The user input as a string
+    """
     user_input = input(prompt)
     if user_input.lower() == EXIT_COMMAND:
-        print("Exiting...")
         exit()
     return user_input
 
+
+
 def read_graph_from_csv(file_path: str) -> Tuple[Dict[str, List[str]], Optional[str]]:
     """
-    Reads the graph data from the specified CSV file and returns it as an
-    adjacency list.
+    Reads the graph from a CSV file and returns it as an adjacency list.
 
-    :param file_path: str - Path to the CSV file containing graph data
-    :return: tuple - A tuple where the first element is an adjacency list
-             represented as a dictionary (or None in case of error), and the
-             second element is an error message (or None if successful)
+    :param file_path: The path to the CSV file
+    :return: A tuple containing the adjacency list and an error message if any
     """
     adjacency_list = {}
     try:
@@ -43,14 +47,12 @@ def read_graph_from_csv(file_path: str) -> Tuple[Dict[str, List[str]], Optional[
 
 
 # Section: User Input Handling
-def get_user_input(graph: Dict[str, List[str]]) -> Tuple[str, str, Dict[str, List[str]], bool, bool, bool]:
+def get_user_input(graph: Dict[str, List[str]]) -> Tuple[str, str, Dict[str, List[str]], bool, bool, bool, bool, bool]:
     """
-    Prompts the user to input necessary information for graph traversal,
-    including the start node, end node, and various flags for printing and
-    visualization.
+    Gets user input for various options in the CLI.
 
-    :param graph: dict - The graph represented as an adjacency list
-    :return: tuple - A tuple containing the start node, end node, graph, and flags entered by the user
+    :param graph: The graph represented as an adjacency list
+    :return: A tuple containing user inputs for various options
     """
     while True:
         try:
@@ -61,23 +63,28 @@ def get_user_input(graph: Dict[str, List[str]]) -> Tuple[str, str, Dict[str, Lis
                 raise ValueError("Node ID out of range")
 
             print_flag_input = get_input(
-                "Enter p to print the graph, or type 'exit' to quit, or press "
-                "Enter to continue: "
+                "Enter p to print the graph, or type 'exit' to quit, or press Enter to continue: "
             )
+
             debug_flag_input = get_input(
-                "Enter d to enable debug mode (verbose output), or type 'exit'"
-                " to quit, or press Enter to continue: "
+                "Enter d to enable debug mode (verbose output), b to debug BFS, f to debug DFS, "
+                "or type 'exit' to quit, or press Enter to continue: "
             )
+            debug_flag = debug_flag_input == 'd'
+            debug_bfs = debug_flag_input == 'b'
+            debug_dfs = debug_flag_input == 'f'
+
             visualize_flag_input = get_input(
-                "Enter v to visualize the graph and the path found, or type "
-                "'exit' to quit, or press Enter to continue: "
+                "Enter v to visualize the graph and the path found, or type 'exit' to quit, or press Enter to continue: "
             )
 
             return (
                 start_node, end_node, graph,
-                print_flag_input == 'p',
-                debug_flag_input == 'd',
-                visualize_flag_input == 'v'
+                print_flag_input.lower() == 'p',
+                debug_flag,
+                visualize_flag_input.lower() == 'v',
+                debug_bfs,
+                debug_dfs
             )
         except ValueError as e:
             print(f"Invalid input: {e}. Please try again.")
@@ -89,6 +96,7 @@ def get_user_input(graph: Dict[str, List[str]]) -> Tuple[str, str, Dict[str, Lis
 def reconstruct_path(predecessor: Dict[str, str], start_node: str, end_node: str) -> Optional[List[str]]:
     """
     Reconstructs the path from start_node to end_node using the predecessor dictionary.
+
     :param predecessor: A dictionary where the key is a node and the value is the predecessor of that node
     :param start_node: The start node for the path
     :param end_node: The end node for the path
@@ -105,16 +113,17 @@ def reconstruct_path(predecessor: Dict[str, str], start_node: str, end_node: str
         return None
 
 
-def bfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool = False) -> Optional[List[str]]:
+def bfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool = False, debug_bfs: bool = False) -> Optional[List[str]]:
     """
     Performs breadth-first search (BFS) on the graph to find the shortest path
     from start_node to end_node.
+
     :param graph: The graph represented as an adjacency list
     :param start_node: The start node for the BFS
     :param end_node: The end node for the BFS
-    :param debug: A flag indicating whether to print debug information
-    :return: A list representing the path from start_node to end_node (or None
-             if no path found)
+    :param debug: A flag indicating whether to print general debug information
+    :param debug_bfs: A flag indicating whether to print BFS specific debug information
+    :return: A list representing the path from start_node to end_node (or None if no path found)
     """
     start_time = time()
     queue = deque([start_node])
@@ -127,11 +136,13 @@ def bfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool
         if len(queue) > MAX_TRAVERSAL_QUEUE_SIZE:
             raise MemoryError("BFS queue size limit exceeded")
 
-        if debug:
-            print(f"Queue: {queue}")
-            print(f"Visited: {visited}")
-
         current_node = queue.popleft()
+        
+        if debug or debug_bfs:
+            print(f"Current Node: {current_node}")
+            print(f"Queue State: {list(queue)}")
+            print(f"Visited Nodes: {visited}")
+
         if current_node == end_node:
             return reconstruct_path(predecessor, start_node, end_node)
         
@@ -144,50 +155,59 @@ def bfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool
     return None
 
 
-def dfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool = False) -> Optional[List[str]]:
+def dfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool = False, debug_dfs: bool = False) -> Optional[List[str]]:
     """
     Performs depth-first search (DFS) on the graph to find a path from
-    start_node to end_node using an explicit stack data structure.
-    
-    :param graph: dict[str, List[str]] - The graph represented as an adjacency list
-    :param start_node: str - The start node for the DFS
-    :param end_node: str - The end node for the DFS
-    :param debug: bool - A flag indicating whether to print debug information
-    :return: List[str] or None - A list representing the path from start_node to end_node or None if no path found
-    """
-    start_time = time()
-    stack = [start_node]
-    visited = {start_node}
-    predecessor = {start_node: None}
+    start_node to end_node.
 
-    while stack:
+    :param graph: The graph represented as an adjacency list
+    :param start_node: The start node for the DFS
+    :param end_node: The end node for the DFS
+    :param debug: A flag indicating whether to print general debug information
+    :param debug_dfs: A flag indicating whether to print DFS specific debug information
+    :return: A list representing the path from start_node to end_node (or None if no path found)
+    """
+    visited = set()
+    predecessor = {}
+    start_time = time()
+    stack_size = 0
+
+    def helper(current_node: str) -> bool:
+        nonlocal stack_size
         if time() - start_time > TRAVERSAL_TIMEOUT_SECONDS:
             raise TimeoutError("DFS timed out")
-        if len(stack) > MAX_TRAVERSAL_QUEUE_SIZE:
+        if stack_size > MAX_TRAVERSAL_QUEUE_SIZE:
             raise MemoryError("DFS stack size limit exceeded")
-
-        current_node = stack.pop()
         
-        if debug:
+        if debug or debug_dfs:
             print(f"Current Node: {current_node}")
             print(f"Visited: {visited}")
-
+        
         if current_node == end_node:
-            return reconstruct_path(predecessor, start_node, end_node)
-
+            return True
+        visited.add(current_node)
+        stack_size += 1
+        
         for neighbor in graph[current_node]:
             if neighbor not in visited:
-                visited.add(neighbor)
-                stack.append(neighbor)
                 predecessor[neighbor] = current_node
+                if helper(neighbor):
+                    return True
+        
+        stack_size -= 1
+        return False
 
-    return None
+    if helper(start_node):
+        return reconstruct_path(predecessor, start_node, end_node)
+    else:
+        return None
 
 
 # Section: Graph Visualization and Printing
 def print_graph(graph: Dict[str, List[str]]) -> None:
     """
     Prints the graph represented as an adjacency list.
+
     :param graph: The graph represented as an adjacency list
     """
     for node, neighbors in graph.items():
@@ -198,6 +218,7 @@ def print_graph(graph: Dict[str, List[str]]) -> None:
 def visualize_graph(graph: Dict[str, List[str]], path: List[str]) -> None:
     """
     Visualizes the graph and the specified path using NetworkX and Matplotlib.
+
     :param graph: The graph represented as an adjacency list
     :param path: A list representing a path in the graph
     """
@@ -240,8 +261,9 @@ def visualize_graph(graph: Dict[str, List[str]], path: List[str]) -> None:
 # Section: Main CLI Function
 def graph_traversal_cli() -> None:
     """
-    The main function that orchestrates the entire process of reading the graph
-    data from a file, getting user input, and performing BFS and DFS.
+    The main function that executes the command line interface for graph traversal.
+
+    It gets user input, reads the graph from a file, and executes graph traversal algorithms.
     """
     while True:
         try:
@@ -249,7 +271,7 @@ def graph_traversal_cli() -> None:
                 "Please enter the file name and extension (or type 'exit' to quit): "
             )
             if not file_name.endswith(CSV_FILE_EXTENSION):
-                print(f"Invalid file extension. Please enter a valid {CSV_EXTENSION} file.")
+                print(f"Invalid file extension. Please enter a valid {CSV_FILE_EXTENSION} file.")
                 continue
 
             graph, error = read_graph_from_csv(file_name)
@@ -258,14 +280,14 @@ def graph_traversal_cli() -> None:
                 continue
 
             user_input_result = get_user_input(graph)
-            start_node, end_node, graph, print_flag, debug_flag, visualize_flag = user_input_result
+            start_node, end_node, graph, print_flag, debug_flag, visualize_flag, debug_bfs, debug_dfs = user_input_result
 
             if print_flag:
                 print("Graph:")
                 print_graph(graph)
 
             print("Loading file...")
-            bfs_result = bfs(graph, start_node, end_node, debug=debug_flag)
+            bfs_result = bfs(graph, start_node, end_node, debug=debug_flag, debug_bfs=debug_bfs)
             if bfs_result:
                 print("Breadth-first traversal")
                 print(" -> ".join(map(str, bfs_result)))
@@ -274,7 +296,7 @@ def graph_traversal_cli() -> None:
             else:
                 print("No path found in breadth-first traversal")
 
-            dfs_result = dfs(graph, start_node, end_node, debug=debug_flag)
+            dfs_result = dfs(graph, start_node, end_node, debug=debug_flag, debug_dfs=debug_dfs)
             if dfs_result:
                 print("Depth-first Search")
                 print(" -> ".join(map(str, dfs_result)))
