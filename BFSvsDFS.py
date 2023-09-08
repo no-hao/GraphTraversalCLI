4e9953491b95f1c3e6fe8595df8e01296b90ba6c
Eric Stine <ericstine@Erics-MacBook-Pro.local>
2023-09-08 04:29:09 -0400

    final changes before modularization.

--- BFSvsDFS.py
+++ BFSvsDFS.py
@@ -9,7 +9,7 @@ CSV_FILE_EXTENSION = '.csv'
 
 # Error handling constants
 MAX_TRAVERSAL_QUEUE_SIZE = 10000  # Adjust as necessary
-TRAVERSAL_TIMEOUT_SECONDS = 900  # Adjust as necessary in seconds
+TRAVERSAL_TIMEOUT_SECONDS = 60  # Adjust as necessary in seconds
 
 
 # Section: Input and Output utilities
@@ -115,8 +115,8 @@ def reconstruct_path(predecessor: Dict[str, str], start_node: str, end_node: str
 
 def bfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool = False, debug_bfs: bool = False) -> Optional[List[str]]:
     """
-    Performs breadth-first search (BFS) on the graph to find the shortest path
-    from start_node to end_node.
+    Performs breadth-first search (BFS) on the graph to find the shortest path from start_node to end_node.
+    Now includes detailed debug information such as current level, queue size, and time elapsed.
 
     :param graph: The graph represented as an adjacency list
     :param start_node: The start node for the BFS
@@ -126,7 +126,7 @@ def bfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool
     :return: A list representing the path from start_node to end_node (or None if no path found)
     """
     start_time = time()
-    queue = deque([start_node])
+    queue = deque([(start_node, 0)])  # Include level information
     visited = set([start_node])
     predecessor = {start_node: None}
 
@@ -136,12 +136,13 @@ def bfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool
         if len(queue) > MAX_TRAVERSAL_QUEUE_SIZE:
             raise MemoryError("BFS queue size limit exceeded")
 
-        current_node = queue.popleft()
-        
+        current_node, level = queue.popleft()
+
         if debug or debug_bfs:
             print(f"Current Node: {current_node}")
-            print(f"Queue State: {list(queue)}")
-            print(f"Visited Nodes: {visited}")
+            print(f"Current Level: {level}")
+            print(f"Queue Size: {len(queue)}")
+            print(f"Time Elapsed: {time() - start_time:.2f} seconds")
 
         if current_node == end_node:
             return reconstruct_path(predecessor, start_node, end_node)
@@ -149,16 +150,16 @@ def bfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool
         for neighbor in graph[current_node]:
             if neighbor not in visited:
                 visited.add(neighbor)
-                queue.append(neighbor)
+                queue.append((neighbor, level+1))
                 predecessor[neighbor] = current_node
 
     return None
 
-
 def dfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool = False, debug_dfs: bool = False) -> Optional[List[str]]:
     """
-    Performs depth-first search (DFS) on the graph to find a path from
-    start_node to end_node.
+    Performs depth-first search (DFS) on the graph to find a path from start_node to end_node.
+    Now includes detailed debug information such as current recursion depth, stack size, and time elapsed,
+    along with information on backtracking steps.
 
     :param graph: The graph represented as an adjacency list
     :param start_node: The start node for the DFS
@@ -172,7 +173,7 @@ def dfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool
     start_time = time()
     stack_size = 0
 
-    def helper(current_node: str) -> bool:
+    def helper(current_node: str, depth: int) -> bool:
         nonlocal stack_size
         if time() - start_time > TRAVERSAL_TIMEOUT_SECONDS:
             raise TimeoutError("DFS timed out")
@@ -181,7 +182,9 @@ def dfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool
         
         if debug or debug_dfs:
             print(f"Current Node: {current_node}")
-            print(f"Visited: {visited}")
+            print(f"Current Recursion Depth: {depth}")
+            print(f"Stack Size: {stack_size}")
+            print(f"Time Elapsed: {time() - start_time:.2f} seconds")
         
         if current_node == end_node:
             return True
@@ -191,13 +194,16 @@ def dfs(graph: Dict[str, List[str]], start_node: str, end_node: str, debug: bool
         for neighbor in graph[current_node]:
             if neighbor not in visited:
                 predecessor[neighbor] = current_node
-                if helper(neighbor):
+                if helper(neighbor, depth+1):
                     return True
+        
+        if debug or debug_dfs:
+            print(f"Backtracking from: {current_node}")
         
         stack_size -= 1
         return False
 
-    if helper(start_node):
+    if helper(start_node, 0):
         return reconstruct_path(predecessor, start_node, end_node)
     else:
         return None
